4a91e527c90624400fbd9f34b87e9046cc920c8c
Tyler_Cox <Tyler_Cox@dell.com>
2017-09-14 10:26:01 -0500

    Fixes docker-build script
    Fixes issues with mistagged images
    
    Closes #39
    
    Signed-off-by: Tyler_Cox <Tyler_Cox@dell.com>

--- shell/docker-build.sh
+++ shell/docker-build.sh
@@ -6,19 +6,15 @@ set -e -o pipefail
 # Switch to the directory where the Dockerfile is
 cd "$DOCKER_ROOT"
 
-# Build the docker image
-
-# Allow word splitting
-# shellcheck disable=SC2086
-docker build $DOCKER_ARGS . | tee "$WORKSPACE/docker_build_log.txt"
-DOCKER_IMAGE=$(grep -Po 'Successfully built \K[^ ]+' \
-    "$WORKSPACE/docker_build_log.txt")
-
 # DOCKERREGISTRY is purposely not using an '_' so as to not conflict with the
 # Jenkins global env var of the DOCKER_REGISTRY which the docker-login step uses
 IMAGE_NAME="$DOCKERREGISTRY/$DOCKER_NAME:$DOCKER_TAG"
 
-docker tag "$DOCKER_IMAGE" "$IMAGE_NAME"
+# Build the docker image
+
+# Allow word splitting
+# shellcheck disable=SC2086
+docker build $DOCKER_ARGS . -t $IMAGE_NAME | tee "$WORKSPACE/docker_build_log.txt"
 
 # Write DOCKER_IMAGE information to a file so it can be injected into the
 # environment for following steps
