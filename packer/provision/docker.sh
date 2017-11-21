bfe04895504eb976b7657a5970b3f47eeb81cccf
Jeremy Phelps <jphelps@linuxfoundation.org>
2017-11-21 13:49:55 -0600

    Install docker-compose
    Install docker-compose as the integration
    tests are instrumented this way currently.
    
    Signed-off-by: Jeremy Phelps <jphelps@linuxfoundation.org>

--- packer/provision/docker.sh
+++ packer/provision/docker.sh
@@ -44,6 +44,11 @@ EOL
 
     # Install python dependencies
     yum install -y python-{devel,virtualenv,setuptools,pip}
+
+    # Install docker-compose per https://docs.docker.com/compose/install/#install-compose
+    sudo curl -L https://github.com/docker/compose/releases/download/1.17.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+    docker-compose -v
 }
 
 ubuntu_changes() {
