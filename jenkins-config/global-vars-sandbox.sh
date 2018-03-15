bc5ba184f8360ba98b5f9bdda5d76c0d5daeb410
Thanh Ha <thanh.ha@linuxfoundation.org>
2018-03-15 16:53:41 -0400

    Add global-vars configuration
    This is in prep for the global-jjb v0.17.0 update.
    
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

---
+++ jenkins-config/global-vars-sandbox.sh
@@ -0,0 +1,9 @@
+DOCKER_REGISTRY=nexus3.edgexfoundry.org
+GIT_BASE=https://github.com/edgexfoundry/$PROJECT
+GIT_CLONE_URL=git@github.com:
+GIT_URL=https://github.com
+JENKINS_HOSTNAME=vex-yul-edgex-jenkins-2
+LOGS_SERVER=https://logs.edgexfoundry.org
+NEXUS_URL=https://nexus.edgexfoundry.org
+REGISTRY_PORTS=10001 10002 10003
+SILO=sandbox
