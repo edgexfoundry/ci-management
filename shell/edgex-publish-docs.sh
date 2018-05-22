644b1c7bb5fd285f50c06bf33c0050e890d6f224
Jeremy Phelps <jphelps@linuxfoundation.org>
2018-05-22 12:27:18 -0500

    Add doc build jobs for edgex-go
    Signed-off-by: Jeremy Phelps <jphelps@linuxfoundation.org>

---
+++ shell/edgex-publish-docs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Ensure we fail the job if any steps fail
+set -ex -o pipefail
+
+# Required Parameters
+
+#   <NEXUS_URL>:      URL of Nexus server. Eg: https://nexus.edgexfoundry.org
+#   <NEXUS_PATH>:     Path on nexus logs repo to place the logs. Eg:
+#                     snapshots/branch/$BUILD_ID
+#                     or
+#                     release/branch
+#   <NEXUS_REPO>:     Name of the nexus repo to use
+#   <DOC_DIRECTORY>:  Absolute path of doc build step output directory.
+
+zip -r docs.zip ${DOC_DIRECTORY}
+lftools deploy nexus-zip ${NEXUS_URL} ${NEXUS_REPO} ${NEXUS_PATH} docs.zip
