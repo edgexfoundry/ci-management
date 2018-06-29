8e5761f263e1f6502e4e9b1eed0841b9b48138cd
Jeremy Phelps <jphelps@linuxfoundation.org>
2018-06-29 15:37:14 -0500

    Only do snapshot strip if DOCKER_TAG not set
    Signed-off-by: Jeremy Phelps <jphelps@linuxfoundation.org>

--- shell/snapshot-strip.sh
+++ shell/snapshot-strip.sh
@@ -2,5 +2,8 @@
 # Ensure we fail the job if any steps fail
 set -e -o pipefail
 
-# Strip -SNAPSHOT from pom
-find . -name "*.xml" -print0 | xargs -0 sed -i 's/-SNAPSHOT//g'
+# Strip -SNAPSHOT from pom if DOCKER_TAG is not set
+if [[ -z "$DOCKER_TAG" ]]; then
+  find . -name "*.xml" -print0 | xargs -0 sed -i 's/-SNAPSHOT//g'
+fi
+
