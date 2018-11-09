392d7d86601058a483cb3b7a34ee01c9b3b9c180
Jeremy Phelps <jphelps@linuxfoundation.org>
2018-11-09 14:11:06 -0600

    Fix security service binary push
    Signed-off-by: Jeremy Phelps <jphelps@linuxfoundation.org>

--- shell/security-api-gateway-go-docker-push.sh
+++ shell/security-api-gateway-go-docker-push.sh
@@ -63,7 +63,7 @@ done
 mkdir -p security-api-gateway-go-$VERSION
 bin_dir=security-api-gateway-go-$VERSION/
 
-go_bins=(core/edgexproxy)
+go_bins=(edgexproxy)
 
 for bin in "${go_bins[@]}"; do
   cp $bin $bin_dir
