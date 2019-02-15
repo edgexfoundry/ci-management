c477f22feb2fd8883c8fc5301f09a6d125784bcc
Jeremy Phelps <iot.engineering@dell.com>
2019-02-14 18:54:11 -0600

    Do not let snapshot strip script fail
    This should pass as there are projects that do not have a pom.xml
    
    Signed-off-by: Jeremy Phelps <iot.engineering@dell.com>

--- shell/snapshot-strip.sh
+++ shell/snapshot-strip.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# Ensure we fail the job if any steps fail
-set -e -o pipefail
+# Do not allow script to fail if no pom.xml exists
+set +e
 
 # Strip -SNAPSHOT from pom if DOCKER_TAG is not set
 if [[ -z "$DOCKER_TAG" ]]; then
   find . -name "*.xml" -print0 | xargs -0 sed -i 's/-SNAPSHOT//g'
 fi
-
+exit 0
