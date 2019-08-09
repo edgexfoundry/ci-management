dbb6e83c370ea4fdae9d8c2f66981fc9ac286a1b
Emilio Reyes <emilio.reyes@intel.com>
2019-08-09 15:04:21 -0700

    fixed builder to enable sysinfo build log
    Signed-off-by: Emilio Reyes <emilio.reyes@intel.com>

--- shell/edgex-infra-ship-logs.sh
+++ shell/edgex-infra-ship-logs.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 echo "---> edgex-infra-ship-logs.sh"
 
+# determine location of sar standard activity daily file
+if [[ -n "$(uname -a | grep Ubuntu)" ]]; then
+    SAR_FILE='/var/log/sysstat'
+else
+    SAR_FILE='/var/log/sa'
+fi
+
 ARCH=$(uname -m)
+
 env | grep -v PATH > .env
 docker container run \
   --privileged \
@@ -11,6 +19,7 @@ docker container run \
   -v $WORKSPACE:$WORKSPACE \
   -v ${WORKSPACE}@tmp:${WORKSPACE}@tmp \
   -v /home/jenkins:/home/jenkins \
+  -v ${SAR_FILE}:/var/log/sa \
   -e SERVER_ID=logs \
   nexus3.edgexfoundry.org:10003/edgex-lftools-log-publisher:${ARCH} \
   sh -c "sh global-jjb/shell/create-netrc.sh && \
