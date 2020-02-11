ba35db24c05e130fc536f23597368778c22cba57
Ernesto Ojeda <ernesto.ojeda@intel.com>
2020-02-11 13:35:47 -0700

    add error checking in swagger scripts
    created new script to wrap multiple call to publish different API versions.
    This will make it easier to migrate this script to pipelines in the future.
    
    Signed-off-by: Ernesto Ojeda <ernesto.ojeda@intel.com>

--- shell/toSwaggerHub.sh
+++ shell/toSwaggerHub.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
 
-apiKey=$1
-apiFolder=$2
-apiVersion=$3
-oasVersion=$4
-isPrivate=$5
-owner=$6
-apiPath="api/openapi/"${apiFolder}
+echo "--> toSwaggerHub.sh"
 
-for file in ${apiPath}/*.yaml; do
-    apiName="$(echo $file | cut -d "/" -f 4 | cut -d "." -f 1)"
-    echo "API_Name:"$apiName
-    apiContent="$(cat ${apiPath}/${apiName}.yaml)"
-    
-    curl -X POST "https://api.swaggerhub.com/apis/${owner}/${apiName}?isPrivate=${isPrivate}&version=${apiVersion}&oas=${oasVersion}&force=true" -H "accept:application/json" -H "Authorization:${apiKey}" -H "Content-Type:application/yaml" -d "${apiContent}"
-done
+publishToSwagger() {
+    apiKey=$1
+    apiFolder=$2
+    apiVersion=$3
+    oasVersion=$4
+    isPrivate=$5
+    owner=$6
+    apiPath="$WORKSPACE/api/openapi/"${apiFolder}
+
+    echo "[toSwaggerHub] Publishing the API Docs [${apiVersion}] to Swagger"
+
+    if [ -d "$apiPath" ]; then
+        for file in "${apiPath}"/*.yaml; do
+            apiName=$(basename "${file}" | cut -d "." -f 1)
+            apiContent=$(cat "${apiPath}/${apiName}".yaml)
+
+            echo "[toSwaggerHub] Publishing API Name [$apiName]"
+
+            echo curl -X POST "https://api.swaggerhub.com/apis/${owner}/${apiName}?isPrivate=${isPrivate}&version=${apiVersion}&oas=${oasVersion}&force=true" \
+                -H "accept:application/json" \
+                -H "Authorization:${apiKey}" \
+                -H "Content-Type:application/yaml" \
+                -d "${apiContent}"
+        done
+    else
+        echo "Could not API Folder [${apiPath}]. Please make sure the API version exists..."
+        exit 1
+    fi
+}
