827c2de0f45eccf0c6642681a948cf7386fa7209
Ernesto Ojeda <ernesto.ojeda@intel.com>
2020-02-13 08:30:42 -0700

    add ARCH check to prevent push from arm64 nodes
    Signed-off-by: Ernesto Ojeda <ernesto.ojeda@intel.com>

--- shell/edgex-publish-swagger.sh
+++ shell/edgex-publish-swagger.sh
@@ -6,16 +6,21 @@ set -e -o pipefail
 
 echo "--> edgex-go-publish-swagger.sh"
 
-API_VERSION=${API_VERSION:-2.0.0}
-API_FOLDER=${API_FOLDER:-v2}
-SWAGGER_DRY_RUN=${SWAGGER_DRY_RUN:-false}
+# if no ARCH is set or ARCH is not arm
+if [ -z "$ARCH" ] || [ "$ARCH" != "arm64" ] ; then
+    API_VERSION=${API_VERSION:-2.0.0}
+    API_FOLDER=${API_FOLDER:-v2}
+    SWAGGER_DRY_RUN=${SWAGGER_DRY_RUN:-false}
 
-SCRIPTS_ROOT="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+    SCRIPTS_ROOT="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-. $SCRIPTS_ROOT/toSwaggerHub.sh
+    . $SCRIPTS_ROOT/toSwaggerHub.sh
 
-OASVERSION='3.0.0'
-ISPRIVATE=true
-OWNER='EdgeXFoundry1'
+    OASVERSION='3.0.0'
+    ISPRIVATE=true
+    OWNER='EdgeXFoundry1'
 
-publishToSwagger "${APIKEY}" "${API_FOLDER}" "${API_VERSION}" "${OASVERSION}" "${ISPRIVATE}" "${OWNER}" "${SWAGGER_DRY_RUN}"
+    publishToSwagger "${APIKEY}" "${API_FOLDER}" "${API_VERSION}" "${OASVERSION}" "${ISPRIVATE}" "${OWNER}" "${SWAGGER_DRY_RUN}"
+else
+    echo "$ARCH not supported...skipping."
+fi
