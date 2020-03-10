f4425948ab395167e4219f1955076f9db50179f8
Ginny Guan <ginny@iotechsys.com>
2020-03-10 16:49:40 +0800

    Add axis to integration-test matrix Project
    Fix #591
    Change FOR_REDIS axis to DATABASE axis
    Add node axis to set node for axes jobs
    
    Signed-off-by: Ginny Guan <ginny@iotechsys.com>

--- shell/integration-test-run.sh
+++ shell/integration-test-run.sh
@@ -2,10 +2,10 @@
 # Ensure we fail the job if any steps fail
 set -e -o pipefail
 
-bin/run.sh -all postman-integration-test
+cd bin; bash ./run.sh -all postman-integration-test
 
-if [ "${FOR_REDIS:=true}" = true ]; then
-     sed -E -i "s/testsuite name=\"/testsuite name=\"redis_/" $(ls bin/testResult/*.xml)
+if [ "${DATABASE:=redis}" = redis ]; then
+     sed -E -i "s/testsuite name=\"/testsuite name=\"redis_/" $(ls testResult/*.xml)
 else
-     sed -E -i "s/testsuite name=\"/testsuite name=\"mongo_/" $(ls bin/testResult/*.xml)
+     sed -E -i "s/testsuite name=\"/testsuite name=\"mongo_/" $(ls testResult/*.xml)
 fi
