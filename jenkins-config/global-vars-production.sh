8046f678490b435ef1853462ac0438f126bef52b
Houa Yang <hyang@contractor.linuxfoundation.org>
2020-05-18 17:20:26 -0500

    Add S3_BUCKET environment variable
    Add S3_BUCKET variable to address "unrecognized macro" error.
    Although the job passes, the error is confusing and unsightly.
    The S3_BUCKET environment variable is needed for log shipping to s3.
    
    Signed-off-by: Houa Yang <hyang@contractor.linuxfoundation.org>

--- jenkins-config/global-vars-production.sh
+++ jenkins-config/global-vars-production.sh
@@ -7,6 +7,7 @@ JENKINS_HOSTNAME=vex-yul-edgex-jenkins-1
 LOGS_SERVER=https://logs.edgexfoundry.org
 NEXUS_URL=https://nexus.edgexfoundry.org
 REGISTRY_PORTS=10001 10002 10003 10004
+S3_BUCKET=
 SIGUL_BRIDGE_IP=10.30.120.3
 SIGUL_KEY=edgex-release-2018
 SILO=production
